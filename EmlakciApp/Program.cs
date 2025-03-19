c87ff122a5f712f95e7a26c3b5c692daac343de6
Hp <Hp@CANAVAR>
2025-03-19 11:02:54 +0300

    Add project files.

---
+++ EmlakciApp/Program.cs
@@ -0,0 +1,82 @@
+﻿using EmlakciLib;
+
+namespace EmlakciApp
+{
+    internal class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+
+            //Ev evim = new Ev();
+            //evim.alan = 160;
+            //evim.semt = "Gazi";
+            //evim.katno = 2;
+            //evim.odasayisi = 4;
+
+            //Console.WriteLine(evim.EvBilgileri());
+
+
+            //Ev evim2 = new Ev { alan= 150, katno = 3, odasayisi = 3, semt = "Çayyolu" };
+
+
+            //Console.WriteLine(evim2.EvBilgileri());
+
+            //var evim3 =new Ev(3,4,200);
+            //Console.WriteLine(evim3.EvBilgileri());
+
+            //Console.WriteLine($" Bellekteki ev sayısı :{Ev.sayac}");
+
+            //evim.odasayisi = int.Parse(Console.ReadLine());
+            //if (evim.odasayisi < 0)
+            //{
+            //    evim.odasayisi=Math.Abs(evim.odasayisi);
+            //}
+
+
+            var evim = new Ev();
+
+            Console.Write("Oda sayısı: ");
+            evim.SetOdaSayisi(int.Parse(Console.ReadLine()));
+            
+
+            Console.Write("Evin alanı :");
+            evim.SetAlan(double.Parse(Console.ReadLine()));
+            
+
+            Console.Write("Evin kat no: ");
+            evim.SetKatNo(int.Parse(Console.ReadLine()));
+
+            Console.Write("Evin semti: ");
+            evim.SetSemt(Console.ReadLine());
+            
+
+            Console.WriteLine($"\n Evin odasayısı: {evim.GetOdaSayisi()}");
+            Console.WriteLine($"\n Evin alanı: {evim.GetAlan()}");
+            Console.WriteLine($"\n Evin kat no: {evim.GetKatNo()}");
+            Console.WriteLine($"\n Evin semti: {evim.GetSemt()}");
+            //int sayi = 5;//set
+            //Console.WriteLine("sayi");//get
+
+
+
+        }
+
+    }
+
+
+
+}
+
+// Ev evim = new Ev();//Ev classından nesne türetildi 
+// evim: Refarans, Nesnelerin bellekdeki adresini tutarlar. Blleğin STACK bölgesinde tutulurlar. 
+// Ev: Class. Classlar aynı zamanda bir veri tipidir.
+// new: Bellekde nesne oluşturmaya yarayan anahtar sözçüktür.
+// Nesne: new anahtar sözcüğü ile HEAP bölgesinde oluştururlar.
+
+// Erişim belirleyiciler (Access Modifiers )
+// Privite: Sadece class içinde erişilebilir üyelerdir .
+// Public: class içinde tanımlanan üyeye her yerden erişim imkanı sağlar.
+// Ctrl + (.) hata düzeltmek için kullanılabilir çözüm yolları sunar.
+
---
+++ EmlakciLib/Ev.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace EmlakciLib
+{
+    public class Ev
+    {
+        public static int sayac = 0;
+        //public Ev(int katno,int odasayisi,  double alan)
+        //{
+        //    this.katno = katno;
+        //    this.odasayisi = odasayisi;
+        //    this.semt = "Kızılay";
+        //    this.alan = alan;
+        //}
+        public Ev(int odasayiyi, int katno, double alan, string semt = "Kızılay")//constructor
+        {
+            this.odasayisi = odasayiyi;
+            this.katno = katno;
+            this.semt = semt;
+            this.alan = alan;
+            sayac++;
+
+        }
+
+        public Ev()
+        {
+            sayac++;
+        }
+
+        
+        
+
+        private int katno;//field
+        private string semt;//Buraya atanan değerler büyük olmalı.
+        private double alan;
+        private int odasayisi;
+
+        public void SetSemt(string semt)
+        {
+            this.semt = semt.ToUpper();
+        }
+        public string GetSemt()
+        {
+            return this.semt;
+        }
+
+
+        public void SetKatNo(int katno)
+        {
+            this.katno = katno;
+        }
+        public int GetKatNo()
+        {
+            return this.katno;
+        }
+
+        public void SetAlan(double alan)
+        {
+            this.alan = alan;
+        }
+        public double GetAlan()
+        {
+            return this.alan;
+        }
+
+        public void SetOdaSayisi(int odasayisi)
+        {
+            this.odasayisi= Math.Abs(odasayisi);//Kapsülleme
+        }
+        public string EvBilgileri()
+        {
+            return $"Alan: {this.alan} \nsemt: {this.semt} \nkatno: {this.katno} \nodasayısı: {this.odasayisi}\n";
+            sayac++;
+        }
+        public int GetOdaSayisi()
+        {
+            return this.odasayisi;
+        }
+            
+
+
+
+
+    }
+}
+//Constructor-yapıcı metodlar
+//Görevleri: Bir class içindeki nesne türetirken çalışır ve class içindeki fieldların varsayılan değer atamalarını yapar
+//Her calssta mutlaka vardır .Varsayılan olarak parametre almazlar. Bu constructorlara default constructor denir.
+//isimleri clas adı ile aynıdır ve farklı isimle tanımlanamazlar.
+//Constructorlar geriye değer döndürmezler.
+//Constructorlar classlardan nesne türerildiğinde çalışan ilk metoddur.
+//ctor code snippeti ile default constructor oluşturulabilir.
+// Bir class içinde satatic olarak tanımlanan üyelere, class adı ile erişilir ve bu üyüler program çalıştığı sürece bellekte kalır. Nesnelerden bağımsız,classa bağlıdır. !!!!!!!!!!!!!!!
+//Kapsülleme(Encapsulation) ilkesi : Class içerisinde yapılan işlerin, class dışından gizlenmesidir. Örn: odasayisi field'ına değer atama işlemi yapmak için öncelikle field private yapıldı, sonrasında bu fielda erişilebilen public SetOdaSayısı(int odasayisi) metodu yazıldı. Bu metod içinde gelen feğerin mutlak değeri alınarak odasayisi fieldına aktarıldı ve bu işlemden bu metodu kullananların bilgisi olmadı.
